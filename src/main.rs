6a2550f8fba7859638afa90e15c5b3e23dd6a2b2
Carlos Navajas <c.navajas4@gmail.com>
2025-04-10 21:59:53 +0200

    chore: new state creation

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ fn main() -> io::Result<()> {
     let git_dir = fs::read_dir("./.git")?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
-
     println!("@ ----> {:?}", git_dir);
-
     Ok(())
 }
 
